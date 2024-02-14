1b0c97a32364dbc5bd12b185639446edc7094030
traian <agent_tygae@yahoo.com>
2024-02-14 14:05:33 +0200

    Adding localisation

--- localisation.py
+++ localisation.py
@@ -1,15 +1,8 @@
-# Import gettext module
 import gettext
 
-# Set the local directory
 appname = 'SR2_bot'
 localedir = './locales'
 
-# Set up Gettext
-en_i18n = gettext.translation(appname, localedir, fallback=True, languages=['en'])
+gettext.translation(appname, localedir, fallback=True, languages=['en']).install()
 
-# Create the "magic" function
-en_i18n.install()
-
-# Translate message
-print(gettext.gettext("Hello World"))
+print(_("Hello World"))
