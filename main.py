6d42ed29f1e53de26234fc6b79efa1409ea7e2be
traian <agent_tygae@yahoo.com>
2024-02-15 16:31:43 +0200

    Added language menu

--- main.py
+++ main.py
@@ -1,46 +1,45 @@
 from localisation import *
+import tldextract
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import (
+    ReplyKeyboardMarkup,
     InlineKeyboardMarkup,
     InlineKeyboardButton,
 )
 
 i18n.install()
 
+lang = "en"
+
 app = Client("botTest", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
 
 @app.on_message(filters.command(["start"]) & filters.private)
 async def startHandler(client, message):
-    await app.send_message(message.chat.id, "Salutare, pentru ajutor tastati tasta /help")
+    await app.send_message(
+        message.chat.id,
+        "Lang",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Romanian", callback_data="ro"),
+                    InlineKeyboardButton("English", callback_data="en"),
+                ]
+            ]
+        ),
+    )
+
+
+@app.on_callback_query()
+async def answer(client, callback_query):
+    lang = callback_query.data
+    print(lang)
 
 
 @app.on_message(filters.command(["help"]) & filters.private)
 async def helpHandler(client, message):
-    await app.send_message(message.chat.id, "Pentru menu tastatzi /menu")
-
-
-@app.on_message(filters.command(["menu"]) & filters.private)
-async def menuHandler(client, message):
-    await app.send_message(
-        message.chat.id,
-        _("Cine este cel mai misto MANELIST?"),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Florin Salam",
-                        url="https://youtu.be/-KBTMvlziBk?feature=shared&t=49",
-                    ),
-                    InlineKeyboardButton("Nicolae guta", url="www.mata.com"),
-                ],
-                [
-                    InlineKeyboardButton("Tzanca Uraganu", url="www.mata.com"),
-                    InlineKeyboardButton("Copilu de Aur", url="www.mata.com"),
-                ],
-            ]
-        ),
-    )
+    await app.send_message(message.chat.id, _("Ati selectat limba romana"))
 
 
 app.run()
